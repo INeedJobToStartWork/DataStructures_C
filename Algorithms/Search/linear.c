275d81d34853d6d9026b45fc440e8a1d1e0c9ed7
ineedjobtostartwork <ineedjobtostartwork@gmail.com>
2024-05-27 14:00:37 +0200

    🎉 Feat(linear.c): add linear search algorithm

---
+++ Algorithms/Search/linear.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include "../../libs/utils.h"
+
+int linearSearch(const int *nums, const int numsSize, const int target)
+{
+    for (int i = 0; i != numsSize; i++)
+    {
+        if (nums[i] == target)
+            return i;
+    }
+    return -1;
+}
+
+int main(void)
+{
+    int arrs[] = {10, 22, 33, 47, 59, 69, 70, 88, 96, 140};
+
+    int arrSize = sizeof(arrs) / sizeof(arrs[0]);
+    printf("%d", linearSearch(arrs, arrSize, 10));
+
+    return 0;
+}
