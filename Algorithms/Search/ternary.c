58143dab93c5d9a452058295c1a8e1fc529577a3
ineedjobtostartwork <ineedjobtostartwork@gmail.com>
2024-05-27 13:40:28 +0200

    🎉 Feat(ternary.c): add ternary search algorithm

--- Algorithms/Search/ternary.c
+++ Algorithms/Search/ternary.c
@@ -1,28 +1,51 @@
 #include <stdio.h>
+#include <math.h>
 #include <stdlib.h>
 #include "../../libs/utils.h"
-// #include "../../utils/printfArray.h"
 
 int ternarySearch(const int *nums, const int numsSize, const int target)
 {
-    int *arr = (int *)malloc(numsSize * sizeof(int));
+    int left = 0, right = numsSize - 1;
 
-    for (int i = 0; i < numsSize; i++)
+    while (left < right)
     {
-        arr[i] = nums[i];
+        int m1 = left + (right - left) / 3;
+        int m2 = right - (right - left) / 3;
+
+        if (nums[m1] == target)
+        {
+            return m1;
+        }
+        else if (nums[m2] == target)
+        {
+            return m2;
+        }
+        else if (nums[m1] > target)
+        {
+            right = m1 - 1;
+        }
+        else if (nums[m2] < target)
+        {
+            left = m2 + 1;
+        }
+        else
+        {
+            left = m1 + 1;
+            right = m2 - 1;
+        }
     }
 
-    free(arr);
     return -1;
 }
 int main(void)
 {
-    int arrs[] = {1, 2, 4};
-    int arrSize = sizeof(arrs) / sizeof(arrs[0]);
-    // ternarySearch(arrs, arrSize, 2);
+    int arrs[] = {10, 22, 33, 47, 59, 69, 70, 88, 96, 140};
 
-    int testArr[] = {4, 2, 3, 1, 6};
-    printf("%d", 4 [testArr]);
+    int arrSize = sizeof(arrs) / sizeof(arrs[0]);
+    printf("%d", ternarySearch(arrs, arrSize, 59));
+
+    // int testArr[] = {4, 2, 3, 1, 6};
+    // printf("%d", 4 [testArr]);
 
     return 0;
 }
